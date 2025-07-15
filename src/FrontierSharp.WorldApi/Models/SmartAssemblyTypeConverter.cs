f8518c2217d1ad49a790e90bbd39b6148728a1f6
Scetrov <git@scetrov.live>
2025-07-15 22:43:31 +0100

    fix(): fix "Unknown" SmartAssemblyType

---
+++ src/FrontierSharp.WorldApi/Models/SmartAssemblyTypeConverter.cs
@@ -0,0 +1,29 @@
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace FrontierSharp.WorldApi.Models;
+
+public class SmartAssemblyTypeConverter : JsonConverter<SmartAssemblyType> {
+    public override SmartAssemblyType Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) {
+        switch (reader.TokenType) {
+            case JsonTokenType.String: {
+                var enumText = reader.GetString();
+                if (Enum.TryParse<SmartAssemblyType>(enumText, ignoreCase: true, out var value)) {
+                    return value;
+                }
+
+                break;
+            }
+            case JsonTokenType.Number when reader.TryGetInt32(out int intValue) && Enum.IsDefined(typeof(SmartAssemblyType), intValue):
+                return (SmartAssemblyType)intValue;
+            default:
+                return SmartAssemblyType.Unknown;
+        }
+
+        return SmartAssemblyType.Unknown; // Fallback
+    }
+
+    public override void Write(Utf8JsonWriter writer, SmartAssemblyType value, JsonSerializerOptions options) {
+        writer.WriteStringValue(value.ToString());
+    }
+}
